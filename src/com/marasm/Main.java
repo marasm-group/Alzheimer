14e600342bd282828ba1b1ab44b40cbae394eb0a
Sergey Rump <wnmsr3u@gmail.com>
2016-02-15 21:30:02 +0300

    no message

--- src/com/marasm/Main.java
+++ src/com/marasm/Main.java
@@ -7,10 +7,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
-public class Main {
-
+public class Main
+{
+    public static String PWD()
+    {
+        return Paths.get(".").toAbsolutePath().normalize().toString()+File.separator;
+    }
     public static void main(String[] args)
     {
         String srcPath;
@@ -26,11 +31,11 @@ public class Main {
         }
         if((srcPath = parser.getSrcPath()) == null)
         {
-            srcPath = "/Users/vhq473/test.alz";
+            srcPath = PWD()+"in.alz";
         }
         if((dstPath = parser.getDstPath()) == null)
         {
-            dstPath = "/Users/vhq473/test.marasm";
+            dstPath = PWD()+"out.marasm";
         }
         author = parser.getAuthor();
 
